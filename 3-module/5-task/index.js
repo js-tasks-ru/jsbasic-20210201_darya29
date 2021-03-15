4e4c0d1b6f29dbd22a7d61c22635b8f665ca4b61
Darya <shm.darya@gmail.com>
2021-03-15 23:14:05 +0300

    making changes to the repo, add 3module/5task, add 4module/all tasks

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let filterNumber = str.split(' ')
+    .join()
+    .split(',')
+    .map((elem) => parseFloat(elem))
+    .filter((elem) => isFinite(elem));
+
+  return {
+    min: Math.min(...filterNumber),
+    max: Math.max(...filterNumber),
+  };
 }
